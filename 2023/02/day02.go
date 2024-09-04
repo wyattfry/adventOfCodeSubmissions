a6595e5ecdabdb84f6559f081cf75b767adab905
Wyatt Fry <wfry@bridgephase.com>
2024-09-04 12:43:17 -0400

    refactor

--- 2023/02/day02.go
+++ 2023/02/day02.go
@@ -7,69 +7,32 @@ import (
 	"strconv"
 )
 
-// type cubeset struct {
-// 	red   int
-// 	green int
-// 	blue  int
-// }
-
-// type game struct {
-// 	id   int
-// 	sets []cubeset
-// }
-
-// func parseGame(gamestring string) game {
-// 	// Game 94: 14 red, 10 green; 15 red; 4 red; 4 green, 7 red, 1 blue; 6 red, 5 green; 1 red, 2 green
-// 	idstring := regexp.MustCompile(`^Game (\d+):`).FindString(gamestring)
-// 	id, _ := strconv.Atoi(idstring)
-
-// 	sets :=
-
-// 	for _, set := range strings.Split(strings.SplitAfterN(gamestring, ":", strings.Index(gamestring, ":"))[1], ";") {
-
-// 		red, _ := strconv.Atoi(regexp.MustCompile(`(\d+) red`).FindString(set))
-// 		blue, _ := strconv.Atoi(regexp.MustCompile(`(\d+) blue`).FindString(set))
-// 		green, _ := strconv.Atoi(regexp.MustCompile(`(\d+) green`).FindString(set))
-
-// 		sets.append(cubeset{
-// 			red:   red,
-// 			blue:  blue,
-// 			green: green,
-// 		})
-
-// 	}
-// 	parsedGame := game{
-// 		id:   id,
-// 		sets: sets,
-// 	}
-
-// 	return parsedGame
-// }
-
-// Determine which games would have been possible if the bag had been loaded
-// with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of
-// the IDs of those games?
-
 var (
-	redCount   = 12
-	greenCount = 13
-	blueCount  = 14
+	colorsCount = map[string]int{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
+	}
 )
 
 func Solve(file string) {
 	var gameIdSum int
 	for index, line := range common.Readlines(file) {
-		if getHighestForColor(line, "red") > redCount {
-			continue
-		} else if getHighestForColor(line, "green") > greenCount {
-			continue
-		} else if getHighestForColor(line, "blue") > blueCount {
-			continue
+		for color, count := range colorsCount {
+			if getHighestForColor(line, color) > count {
+				continue
+			}
 		}
 		gameId := index + 1
 		gameIdSum += gameId
 	}
 	fmt.Println("Solution for part 1:", gameIdSum)
+
+	var powerOfSetsSum int
+	for _, line := range common.Readlines(file) {
+		powerOfSetsSum += getPowerOfSet(line)
+	}
+	fmt.Println("Solution for part 2:", powerOfSetsSum)
 }
 
 func getHighestForColor(gamestring string, color string) int {
@@ -82,7 +45,14 @@ func getHighestForColor(gamestring string, color string) int {
 			}
 		}
 	}
-	fmt.Println(matches, "highest:", highest)
 
 	return highest
+}
+
+func getPowerOfSet(gamestring string) int {
+	powerOfSet := 1
+	for color := range colorsCount {
+		powerOfSet *= getHighestForColor(gamestring, color)
+	}
+	return powerOfSet
 }
--- 2023/02/day02_test.go
+++ 2023/02/day02_test.go
@@ -21,7 +21,7 @@ func runTestCases(t *testing.T, testName string, cases []testCase, testFunc func
 	}
 }
 
-func TestGetHighestForColor(t *testing.T) {
+func TestGetHighestForColorRed(t *testing.T) {
 	cases := []testCase{
 		{input: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", output: 4},
 		{input: "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", output: 1},
@@ -29,4 +29,32 @@ func TestGetHighestForColor(t *testing.T) {
 	runTestCases(t, "stringToInt", cases, func(s string) int {
 		return getHighestForColor(s, "red")
 	})
+}
+
+func TestGetHighestForColorBlue(t *testing.T) {
+	cases := []testCase{
+		{input: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", output: 6},
+		{input: "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", output: 4},
+	}
+	runTestCases(t, "stringToInt", cases, func(s string) int {
+		return getHighestForColor(s, "blue")
+	})
+}
+
+func TestGetHighestForColorGreen(t *testing.T) {
+	cases := []testCase{
+		{input: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", output: 2},
+		{input: "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", output: 3},
+	}
+	runTestCases(t, "stringToInt", cases, func(s string) int {
+		return getHighestForColor(s, "green")
+	})
+}
+
+func TestGetPowerOfSet(t *testing.T) {
+	cases := []testCase{
+		{input: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", output: 4 * 2 * 6},
+		{input: "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", output: 4 * 3 * 1},
+	}
+	runTestCases(t, "stringToInt", cases, getPowerOfSet)
 }
