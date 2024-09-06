ec34ce914d0ea2dfb34a6e5df36f2645d2eb4ac6
Wyatt Fry <wfry@bridgephase.com>
2024-09-06 12:09:18 -0400

    added project template

---
+++ 2023/template/day02/day02.go
@@ -0,0 +1,31 @@
+package day02
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func Solve() {
+	lines := readlines("./day02/input.txt")
+
+	// Your Solution Here
+	// `lines` is a string array of the input text file
+
+	fmt.Println(lines)
+}
+
+func readlines(fileName string) []string {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
+}
---
+++ 2023/template/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"aoc/template/day01"
+	"aoc/template/day02"
+)
+
+func main() {
+	day01.Solve()
+	day02.Solve()
+}
